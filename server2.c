97ac4b2a6e666e3fb56a99fa27925d8815a9a921
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2018-12-04 16:13:34 -0200

    Divisão do código de servidor em funções

--- server2.c
+++ server2.c
@@ -15,17 +15,46 @@ int cmpfunc(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
 }
 
-// Driver code
-int main()
+void print(char string[][MAXLINE], int i)
 {
+    int j;
+    for (j = 0; j < i; j++) {
+        printf("%s\n", string[j]);
+    }
+}
+
+void takeofFirstChar(char string[][MAXLINE], int i) {
+    int j;
+    int k;
+    for (j = 0; j <= i; j++) {
+        for (k = 0; k < strlen(string[j]); k++) {
+            string[j][k] = string[j][k + 1];
+        }
+        string[j][k] = '\0';
+    }
+}
+
+int trimString(char buffer[], char output[][MAXLINE]) {
+    char *p;
+    int i = 0;
+    p = buffer;
+    while (*p != '\0') {
+        strcpy(output[i++], p);
+        p += strlen(p) + 1;
+    }
+    return i;
+}
+
+// Driver code
+int main() {
+
     int sockfd;
     char buffer[MAXLINE];
     char *hello = "Hello from server";
     struct sockaddr_in servaddr, cliaddr;
 
     // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -40,8 +69,7 @@ int main()
 
     // Bind the socket with the server address
     if (bind(sockfd, (const struct sockaddr *)&servaddr,
-             sizeof(servaddr)) < 0)
-    {
+             sizeof(servaddr)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
@@ -50,32 +78,17 @@ int main()
     n = recvfrom(sockfd, (char *)buffer, MAXLINE,
                  MSG_WAITALL, (struct sockaddr *)&cliaddr,
                  &len);
+    
     buffer[n] = '\0';
-    char output[150][150];
+    char output[MAXLINE][MAXLINE];
 
-    char *p;
-    int i = 0;
-    p = buffer;
-    while (*p != '\0')
-    {
-        strcpy(output[i++], p);
-        p += strlen(p) + 1;
-    }
+    int i = trimString(buffer, output);
 
-    qsort(output, i, 150, cmpfunc);
+    qsort(output, i, MAXLINE, cmpfunc);
 
-    int j;
-    int k;
-    for (j = 0; j <= i; j++) {
-        for(k = 0; k < strlen(output[j]); k++) {
-            output[j][k] = output[j][k + 1];
-        }
-        output[j][k] = '\0';
-    }
+    takeofFirstChar(output, i);
 
-    for (j = 0; j < i; j++) {
-        printf("%s\n", output[j]);
-    }
+    print(output, i);
 
     sendto(sockfd, (const char *)hello, strlen(hello),
            MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
